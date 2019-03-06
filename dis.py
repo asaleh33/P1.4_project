8c1a2dba3ec3a0ff57bc7a882e6ebd2aa0467f4f
asaleh33 <salehastro@gmail.com>
2019-03-06 18:31:03 +0100

    three assignments

---
+++ dis.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+
+def Dinfty(a,b):
+    '''
+    dinfty
+    '''
+    return abs(a,b).max(0,1)
+
+def D1(a,b):
+    '''
+    d_one
+    '''
+    return sum(abs(b-a))     
+
+def D2(a,b):
+    '''
+    d_two
+    '''
+    return np.sqrt(sum(abs(b-a))**2)
+
+
+def distmatrix(N, dist, x_train):
+    D = np.zeros((N,N))
+    for i, j in itertools.product(xrange(N), xrange(i,N)):
+        D[i][j] = dist(x_train[i], x_train[j])
+        D[j][i] = D[i][j]
+    return D	  	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
